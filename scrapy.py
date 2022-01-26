bb8545fe50aaf433df710426ab1f3e29ace51375
Zeeshan <mohdzeeshankhan95@gmail.com>
2022-01-26 16:13:26 +0530

    Added Scraper with Scrapy

---
+++ scrapy.py
@@ -0,0 +1,22 @@
+from unittest.mock import call
+import scrapy
+
+
+class NewsSpider(scrapy.Spider):
+
+    name = "news"
+
+    def start_requests(self):
+        urls = [
+            'https://www.bbc.com/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'news.html {page}'
+
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
